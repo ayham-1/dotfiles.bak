0e5094e158034c3d2a1f80aa8b90abd9d95c9937
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-03-31 09:07:07 +0300

    Add st emacs support

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,18 +13,8 @@
   (package-install 'use-package))
   (require 'use-package)
 
+;; Fix st support.
+(add-to-list 'term-file-aliases
+    '("st-256color" . "xterm-256color"))
+
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(flycheck projectile company monokai-theme use-package magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
